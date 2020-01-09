63f04066a33591d0ae9c16d23fcdc7e1d7e98a84
nidingbo <dingbo.ni@dtp1688.com>
2020-01-09 17:25:47 +0800

    no message

---
+++ src/test.java
@@ -0,0 +1,20 @@
+/**
+ * @author NiDingbo
+ * @date 2020/1/9
+ * @des
+ */
+
+public class test {
+    class Person {
+
+        String name = "do1";
+        String age = "12";
+
+        public Person(String name) {
+            this.name = name;
+        }
+    }
+
+
+
+}
